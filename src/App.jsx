d23740fe498caac1a2c41eeed01bc4c1fa12a1d8
Paul Grossetête <paulgrossetete.code@gmail.com>
2025-07-25 09:54:50 +0200

    Ajout de la fonction pour déconnecter l'utilisateur

--- src/App.jsx
+++ src/App.jsx
@@ -73,6 +73,7 @@ const App = () => {
         : <DisplayBlogs
             blogs={blogs}
             user={user}
+            setUser={setUser}
           />
       }
     </div>
--- src/components/DisplayBlogs.jsx
+++ src/components/DisplayBlogs.jsx
@@ -1,13 +1,26 @@
-import Blog from './Blog'
+import Blog from "./Blog";
 
-const DisplayBlogs = ({ blogs, user }) => (
+const DisplayBlogs = ({ blogs, user, setUser }) => {
+
+  const logout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  };
+
+  return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} is logged in</p>
-      {blogs.map(blog =>
+      <div style={{ margin: "10px auto" }}>
+        <span>{user.name} is logged in —</span>
+        <button style={{ marginLeft: 4 }} onClick={logout}>
+          logout
+        </button>
+      </div>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}  
+      ))}
     </div>
-  )
+  );
+};
 
-export default DisplayBlogs
+export default DisplayBlogs;
