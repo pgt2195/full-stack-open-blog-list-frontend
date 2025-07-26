3cc3dfdc708a7b103be5a28d7c6dbd7b30baa2cd
Paul Grossetête <paulgrossetete.code@gmail.com>
2025-07-26 19:55:48 +0200

    Refactoristation de la logique du composant AddBlogs

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -1,4 +1,40 @@
-const AddBlog = ({ addBlog, newBlog, handleNewBlogChange }) => {
+import { useState } from 'react';
+import blogService from '../services/blogs'
+import { emptyBlog, displayMessage } from '../services/utils'
+
+const AddBlog = ({ blogs, setBlogs, setMessage, setErrorMessage }) => {
+  const [newBlog, setNewBlog] = useState(emptyBlog)
+
+  /**
+   * Gère l'ajout d'un nouveau blog depuis le formulaire d'ajout
+   * du composant DisplayBlogs
+   */
+  const addBlog = (event) => {
+    event.preventDefault()
+
+    try {
+      blogService
+        .create(newBlog)
+          .then(returnedBlog => {
+          setBlogs(blogs.concat(returnedBlog))
+          setNewBlog(emptyBlog)
+          displayMessage(`New blog "${newBlog.title}" by <i>${newBlog.author}</i> has been added`, setMessage)
+        })
+    } catch (exception) {
+      displayMessage(`Oops, something wrong happened! Error: ${exception}`, setErrorMessage)
+    }
+  }
+
+  /**
+   * Gère la mise à jour de l'état d'un nouveau blog lorsqu'il 
+   * est en train d'être ajouté
+   */
+  const handleNewBlogChange = (event) => {
+    setNewBlog({
+      ...newBlog,
+      [event.target.name]: event.target.value
+    });
+  }
 
   return (
     <div>
